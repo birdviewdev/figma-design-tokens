80d454f6e13b5c4da4686b48c648ea21e2034853
squll1 <squll1@users.noreply.github.com>
2023-03-06 06:37:06 +0000

    Tokens updated

--- build/ios-swift/StyleDictionarySize.swift
+++ build/ios-swift/StyleDictionarySize.swift
@@ -4,16 +4,34 @@
 //
 
 // Do not edit directly
-// Generated on Fri, 15 Jul 2022 19:49:49 GMT
+// Generated on Mon, 06 Mar 2023 06:37:05 GMT
 
 
 import UIKit
 
 public enum StyleDictionarySize {
-    public static let bodyExtraBoldCondensedItalicFontWeight = 800
-    public static let bodyH3FontWeight = 700
-    public static let bodyH4StrikeThroughFontWeight = 500
-    public static let bodyItalicFontWeight = 400
-    public static let bodyMediumExtendedItalicFontWeight = 500
-    public static let bodySuperFontWeight = 900
+    public static let bodyL15RegularFontWeight = 400
+    public static let bodyM14RegularFontWeight = 400
+    public static let bodyS13RegularFontWeight = 400
+    public static let bodyXl16RegularFontWeight = 400
+    public static let captionS12BoldFontWeight = 700
+    public static let captionS12RegularFontWeight = 400
+    public static let captionXs10BoldFontWeight = 700
+    public static let captionXs10RegularFontWeight = 400
+    public static let labelL15BoldFontWeight = 700
+    public static let labelL15RegularFontWeight = 400
+    public static let labelM14BoldFontWeight = 700
+    public static let labelM14RegularFontWeight = 400
+    public static let labelS12BoldFontWeight = 700
+    public static let labelS12RegularFontWeight = 400
+    public static let labelXl16BoldFontWeight = 700
+    public static let labelXl16RegularFontWeight = 400
+    public static let subtitleL13BoldFontWeight = 700
+    public static let subtitleXl14BoldFontWeight = 700
+    public static let subtitleXxl16BoldFontWeight = 700
+    public static let titleL24SemiboldFontWeight = 600
+    public static let titleM20SemiboldFontWeight = 600
+    public static let titleS18SemiboldFontWeight = 600
+    public static let titleXl28SemiboldFontWeight = 600
+    public static let titleXxl32SemiboldFontWeight = 600
 }
